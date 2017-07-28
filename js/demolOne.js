f2910c43b62a93b1132d452c13bea016c40f7096
comer <nova_xinlai@126.com>
2017-07-28 15:18:13 +0800

    打印promise对象

--- js/demolOne.js
+++ js/demolOne.js
@@ -1,3 +1,8 @@
+/**
+ * 1.resolve 如果执行成功，在then中进行捕获
+ * 使用then这种链式操作方法，减少回调函数的层层嵌套形式
+ * @return {[type]} [description]
+ */
 function runAsync1(){
     var p = new Promise(function(resolve, reject){
         //做一些异步操作
@@ -41,3 +46,66 @@ runAsync1()
 .then(function(data){
     console.log(data);
 });
+
+
+/**
+ * 2.既可以返回Promise对象，也可以返回数据
+ */
+
+/*runAsync1()
+.then(function(data){
+    console.log(data);
+    return runAsync2();
+})
+.then(function(data){
+    console.log(data);
+    return 'hello world';  //这里直接返回数据
+})
+.then(function(data){
+    console.log(data);
+});*/
+
+/**
+ * 3.扩展：将方法1的一个成功回调的数据返回给方法3
+ */
+var a,b;
+function a1(){
+    var p = new Promise(function(resolve, reject){
+        //做一些异步操作
+        setTimeout(function(){
+            resolve('a');
+        }, 1000);
+    });
+    return p;            
+}
+function b1(data){
+    var p = new Promise(function(resolve, reject){
+        //做一些异步操作
+        setTimeout(function(){
+            resolve(data);
+        }, 1000);
+    });
+    return p;            
+}
+function c1(){
+    var p = new Promise(function(resolve, reject){
+        //做一些异步操作
+        setTimeout(function(){
+            resolve('c');
+        }, 1000);
+    });
+    return p;            
+}
+
+/*a1()
+.then(function(data){
+    console.log(data);
+    return b1(data);
+})
+.then(function(data){
+    console.log(data);
+    return c1(data);
+})
+.then(function(data){
+    console.log(data);
+});*/
